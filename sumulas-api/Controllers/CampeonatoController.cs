f1c3e50e7071400fa7c9545ebb56e06dc52b73bb
Felipe Oliveira <44898703+felipedeviterbo@users.noreply.github.com>
2019-04-02 17:01:13 -0300

    Working local DB
    Sumulas.api - Working Local DB

--- sumulas-api/Controllers/CampeonatoController.cs
+++ sumulas-api/Controllers/CampeonatoController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -25,5 +26,51 @@ namespace sumulas.api.Controllers
         {
             return _campeonatoService.Get().ToList();
         }
+
+        //[HttpPost]
+        //[ProducesResponseType(typeof(CampeonatoModel), (int)HttpStatusCode.OK)]
+        //public async Task<IActionResult> Insert([FromBody]CampeonatoModel campeonato)
+        //{
+        //    try
+        //    {
+        //        campeonato = await _campeonatoService.Insert(campeonato);
+        //        return Ok(campeonato);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        ExceptionViewModel vm = new ExceptionViewModel(LevelException.BusinessRule, ex.Message, HttpStatusCode.BadRequest);
+        //        return BadRequest(vm);
+        //    }
+        //}
+
+        [HttpPost]
+        [ProducesResponseType(typeof(CampeonatoModel), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(CampeonatoModel), (int)HttpStatusCode.BadRequest)]
+        public async Task<CampeonatoModel> Insert([FromBody]CampeonatoModel campeonato)
+        {
+            campeonato = await _campeonatoService.Insert(campeonato);
+            return campeonato;
+            
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(CampeonatoModel), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(CampeonatoModel), (int)HttpStatusCode.NotFound)]
+        public async Task<CampeonatoModel> Update(CampeonatoModel campeonato)
+        {
+            campeonato = await _campeonatoService.Update(campeonato);
+            return campeonato;
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(CampeonatoModel), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(CampeonatoModel), (int)HttpStatusCode.NotFound)]
+        public async Task<bool> Delete(CampeonatoModel campeonato)
+        {
+            return await _campeonatoService.Delete(campeonato);
+        }
+
+        // CampeonatoModel Update(CampeonatoModel campeonato);
+        //bool Delete(CampeonatoModel campeonato);
     }
 }
